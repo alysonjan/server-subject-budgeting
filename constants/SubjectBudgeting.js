ad103bb5c7bd1391f74a40f5042a4e1252723d6a
alysonjan <alysonjan@gmail.com>
2021-10-14 22:58:34 +0800

    refactor: totalTeachinghours variable and rad hours

--- constants/SubjectBudgeting.js
+++ constants/SubjectBudgeting.js
@@ -1,4 +1,6 @@
 const RegularSize = 50;
 const AllowanceSize = 11;
+const TotalHoursPerWeek = 24;
+const RadHours = 70;
 
-module.exports = {RegularSize, AllowanceSize}
+module.exports = {RegularSize, AllowanceSize, TotalHoursPerWeek, RadHours}
