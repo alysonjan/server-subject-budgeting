239b912816ac8d93e85d19d0f50af62c4daff875
alysonjan <alysonjan@gmail.com>
2021-10-28 21:00:42 +0800

    feature: fetching generated budget data

--- routes/api/queries/Query.js
+++ routes/api/queries/Query.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// const { check, validationResult } = require('express-validator');
 const db = require('../../../config/database');
 const auth = require('../../../middleware/auth');
 
 router.get('/curriculum', auth, async(req,res)=>{
     try {
-        const sqlSelectCurriculum = "SELECT DISTINCT curriculum_code,year_level FROM curriculum";
+        const sqlSelectCurriculum = "SELECT DISTINCT curriculum_code,year_level FROM curriculum ORDER BY year_level";
         db.query(sqlSelectCurriculum, (err, result) =>{
             if (err){
                 res.status(400).json({ errors: [{ msg: 'Oops, Something went wrong'}] });  
@@ -20,6 +19,21 @@ router.get('/curriculum', auth, async(req,res)=>{
     }
 });
 
+router.get('/subject-budget', auth, async(req,res) => {
+    try {
+        const sqlSelectSubjectBudget = "SELECT * FROM generate_subject";
+        db.query(sqlSelectSubjectBudget, (err, result)=> {
+            if (err){
+                res.status(400).json({ errors: [{ msg: 'Oops, Something went wrong'}] });  
+            }else{
+                res.json(result)
+            }
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 
 module.exports = router;
